639b4918ce34222a0889bc6c3f8cc9636b0c0069
Toey <71362543+the1999@users.noreply.github.com>
2021-03-05 17:11:33 +0700

    Initial...

--- bot.php
+++ bot.php
@@ -1,3 +1,24 @@
 <?php
-echo OK;
+$LINEData = file_get_contents('php://input');
+$jsonData = json_decode($LINEData,true);
+
+$replyToken = $jsonData["events"][0]["replyToken"];
+$userID = $jsonData["events"][0]["source"]["userId"];
+$text = $jsonData["events"][0]["message"]["text"];
+$date = $jsonData["events"][0]["date"];
+
+function sendMessage($replyJson, $sendInfo){
+   $ch = curl_init($sendInfo["URL"]);
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+   curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+   curl_setopt($ch, CURLOPT_POST, true);
+   curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+       'Content-Type: application/json',
+       'Authorization: Bearer ' . $sendInfo["AccessToken"])
+       );
+   curl_setopt($ch, CURLOPT_POSTFIELDS, $replyJson);
+   $result = curl_exec($ch);
+   curl_close($ch);
+   return $result;
+}
 ?>
