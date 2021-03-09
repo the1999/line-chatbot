de3b8439b55810e6fa5182568fda9a445c438aba
Toey <71362543+the1999@users.noreply.github.com>
2021-03-09 12:35:23 +0700

    update3

--- connect.php
+++ connect.php
@@ -1,7 +1,5 @@
 <?php
-
-function connectDB() {
-
+try {
     $myPDO = new PDO("pgsql:host=ec2-52-44-31-100.compute-1.amazonaws.com;dbname=d9gbnhqsslvumi;user=cvjvcfygarrxpt;password=0f79bcb74f6de687639e6a9f73aa0cbd4ea4f21bfaecea939bc1afc3b9db0621");
     $sql = "SELECT * FROM tb_user";
 
@@ -10,16 +8,10 @@ function connectDB() {
         print $row['d_id'].$row['d_name'].'<br/>';
     }
 
+    
+} catch(PDOException $e) {
+    echo $e->getMessage();
 }
-
-if(connectDB()) {
-    echo "connected";
-} else {
-    echo "failed";
-}
-
-
-
 
 
 ?>
