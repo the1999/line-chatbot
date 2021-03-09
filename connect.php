c5882301fe2cc1101e529c4a9f8f3355dca4f625
Toey <71362543+the1999@users.noreply.github.com>
2021-03-09 12:27:34 +0700

    update2

--- connect.php
+++ connect.php
@@ -3,6 +3,12 @@
 function connectDB() {
 
     $myPDO = new PDO("pgsql:host=ec2-52-44-31-100.compute-1.amazonaws.com;dbname=d9gbnhqsslvumi;user=cvjvcfygarrxpt;password=0f79bcb74f6de687639e6a9f73aa0cbd4ea4f21bfaecea939bc1afc3b9db0621");
+    $sql = "SELECT * FROM tb_user";
+
+    foreach($myPDO->query($sql) as $row) {
+        print "<br/>";
+        print $row['d_id'].$row['d_name'].'<br/>';
+    }
 
 }
 
