3a7b209a342a957e64186f47dffc18d645843cd5
Toey <71362543+the1999@users.noreply.github.com>
2021-03-09 13:05:39 +0700

    update9

--- bot.php
+++ bot.php
@@ -1,6 +1,6 @@
 <?php
 require_once ('vendor/autoload.php');
-require_once ('connect.php');
+// require_once ('connect.php');
 $LINEData = file_get_contents('php://input');
 $jsonData = json_decode($LINEData,true);
 
@@ -23,6 +23,16 @@ function sendMessage($replyJson, $sendInfo){
    curl_close($ch);
    return $result;
 }
+
+try {
+    $myPDO = new PDO("pgsql:host=ec2-52-44-31-100.compute-1.amazonaws.com;dbname=d9gbnhqsslvumi;user=cvjvcfygarrxpt;password=0f79bcb74f6de687639e6a9f73aa0cbd4ea4f21bfaecea939bc1afc3b9db0621");
+    
+    $sql = "SELECT * FROM tb_leave";
+
+    foreach($myPDO->query($sql) as $row) {
+        print "<br/>";
+        print $row["d_date"].$row["d_detail"].'<br/>';
+  
 $val = (explode(",",$text));
 
 if ($text == "ดูรายการหน่อย"){
@@ -42,35 +52,39 @@ else if ($text == "1") {
 } 
 
 else if (preg_match("/^((((19|[2-9]\d)\d{2})\-(0[13578]|1[02])\-(0[1-9]|[12]\d|3[01]))|(((19|[2-9]\d)\d{2})\-(0[13456789]|1[012])\-(0[1-9]|[12]\d|30))|(((19|[2-9]\d)\d{2})\-02\-(0[1-9]|1\d|2[0-8]))|(((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))\-02\-29))$/",$val[0])) {
-    $sql_query1 = "INSERT INTO tb_leave (d_date, d_detail) VALUES ('".$val[0]."',".$val[1].")";
-    $myPDO->query($sql_query1);
+    $sql1 = "INSERT INTO tb_leave (d_date, d_detail) VALUES ('".$val[0]."',".$val[1].")";
+    $myPDO->query($sql1);
         $message = '{
             "type" : "text",
             "text" : "บันทึกเรียบร้อย"
         }';
         $replyText = json_decode($message);   
 } 
-// else if ($text == "ดูรายชื่อพนักงาน") {
-//     $sql_query2 = "SELECT * FROM tb_user";
-//     $myPDO->query($sql_query2);
-//     foreach($myPDO->query($sql) as $row) {
-//         $ID1 = [1]['d_id'];
-//         $ID2 = [2]['d_id'];   
-//         $ID3 = [3]['d_id']; 
-//         $ID4 = [4]['d_id']; 
-//         $d_name1 = [1]['d_name'];
-//         $d_name2 = [2]['d_name'];
-//         $d_name3 = [3]['d_name'];
-//         $d_name4 = [4]['d_name'];
-//         $row1 = $row1.$d_name1;
-//         $row2 = $row2.$d_name2;
-//         $row3 = $row3.$d_name3;
-//         $row4 = $row4.$d_name4;
-//         }
-//     $replyText["type"] = "text";
-//     $replyText["text"] = "$row1.$row2.$row3.$row4";
-// }
+else if ($text == "ดูรายชื่อพนักงาน") {
+    $sql_query2 = "SELECT * FROM tb_user";
+    $myPDO->query($sql_query2);
+    foreach($myPDO->query($sql) as $row) {
+        $ID1 = [1]['d_id'];
+        $ID2 = [2]['d_id'];   
+        $ID3 = [3]['d_id']; 
+        $ID4 = [4]['d_id']; 
+        $d_name1 = [1]['d_name'];
+        $d_name2 = [2]['d_name'];
+        $d_name3 = [3]['d_name'];
+        $d_name4 = [4]['d_name'];
+        $row1 = $row1.$d_name1;
+        $row2 = $row2.$d_name2;
+        $row3 = $row3.$d_name3;
+        $row4 = $row4.$d_name4;
+        }
+    $replyText["type"] = "text";
+    $replyText["text"] = "$row1.$row2.$row3.$row4";
+}
 
+} 
+} catch(PDOException $e) {
+    echo $e->getMessage();
+}
 $lineData['URL'] = "https://api.line.me/v2/bot/message/reply";
 $lineData['AccessToken'] = "t6aLTUxPu8V6uO+Mk51mAbUhzXglRM0SXXbxb4SVhFp+04unUqFmNz34MWQyQTao/SQJy+euTHs/s35Y45+N7B+p4PMLoHm63lrTwScrVyqhrQlKqY3BzU/tASZMxYO9X1khaUIMHKCxgER1V1W3AAdB04t89/1O/w1cDnyilFU=";
 
