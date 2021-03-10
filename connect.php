517a45988fbeb4d4f35e597517c69d5bd0b084d1
Toey <71362543+the1999@users.noreply.github.com>
2021-03-10 16:38:46 +0700

    git up24

--- connect.php
+++ connect.php
@@ -3,10 +3,10 @@
 try {
     $myPDO = new PDO("pgsql:host=ec2-52-44-31-100.compute-1.amazonaws.com;dbname=d9gbnhqsslvumi;user=cvjvcfygarrxpt;password=0f79bcb74f6de687639e6a9f73aa0cbd4ea4f21bfaecea939bc1afc3b9db0621");
     
-    $sql = "SELECT * FROM tb_leave";
+    $sql = "SELECT * FROM tb_user";
     foreach($myPDO->query($sql) as $row) {
         print "<br/>";
-        print $row["d_date"].$row["d_detail"].'<br/>';
+        print $row["d_id"].$row["d_name"].'<br/>';
     }
 } catch(PDOException $e) {
     echo $e->getMessage();
