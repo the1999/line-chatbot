66cd9c3040bd20a98e2b92775291cbacae15bd42
Toey <71362543+the1999@users.noreply.github.com>
2021-03-10 16:34:30 +0700

    git up22

--- bot.php
+++ bot.php
@@ -53,20 +53,33 @@ else if (preg_match("/^((((19|[2-9]\d)\d{2})\-(0[13578]|1[02])\-(0[1-9]|[12]\d|3
         $replyText = json_decode($message);   
        
 } 
-else if ($text == "ดูรายชื่อพนักงาน") {
-    $sql_query2 = "SELECT * FROM tb_user";
-    $myPDO->query($sql_query2);
-    $d_id1 = $row[1]['d_id']['d_name'];
-    $d_id2 = $row[2]['d_id']['d_name'];
-    $d_id3 = $row[3]['d_id']['d_name'];
-    $d_id4 = $row[4]['d_id']['d_name'];
-    $arr =  array('$d_id1'.'$d_id2'.'$d_id3'.'$d_id4');
-    foreach ($arr as $row) {
-        $row .= $arr;
-    } 
-
+else if ($text == "ดูรายชื่อพนักงาน")
+$sql_query2 = "SELECT * FROM tb_user";
+$myPDO->query($sql_query2);
+while($row=pg_fetch_assoc($result)) {
+    $row1 = $row[1]['d_id']['d_name'];
+    $row2 = $row[2]['d_id']['d_name'];
+    $row3 = $row[3]['d_id']['d_name'];
+    $row4 = $row[4]['d_id']['d_name'];
+    $row = $row1.$row2.$row3.$row4;
+    
     $replyText["type"] = "text";
     $replyText["text"] = "$row";
+}
+// else if ($text == "ดูรายชื่อพนักงาน") {
+//     $sql_query2 = "SELECT * FROM tb_user";
+//     $myPDO->query($sql_query2);
+//     $d_id1 = $row[1]['d_id']['d_name'];
+//     $d_id2 = $row[2]['d_id']['d_name'];
+//     $d_id3 = $row[3]['d_id']['d_name'];
+//     $d_id4 = $row[4]['d_id']['d_name'];
+//     $arr =  array('$d_id1'.'$d_id2'.'$d_id3'.'$d_id4');
+//     foreach ($arr as $row) {
+//         $row .= $arr;
+//     } 
+
+//     $replyText["type"] = "text";
+//     $replyText["text"] = "$row";
 //     foreach ($id as $value) { 
 //     $d_id1 = [1]['d_id']['d_name'];
 //     $d_id2 = [2]['d_id']['d_name'];
