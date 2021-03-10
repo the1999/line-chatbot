ffeffb2f9390a7114ada7795fc9558093d60b99e
Toey <71362543+the1999@users.noreply.github.com>
2021-03-10 09:15:52 +0700

    update28

--- bot.php
+++ bot.php
@@ -57,14 +57,14 @@ else if ($text == "ดูรายชื่อพนักงาน") {
     $sql_query2 = "SELECT * FROM tb_user";
     $myPDO->query($sql_query2);
     foreach($myPDO->query($sql_query2) as $row) {
-        $d_id1 = $row[1]['d_id'];
-        $d_id2 = $row[2]['d_id'];
-        $d_id3 = $row[3]['d_id'];
-        $d_id4 = $row[4]['d_id'];
-        $d_name1 = $row[1]['d_name'];
-        $d_name2 = $row[2]['d_name'];
-        $d_name3 = $row[3]['d_name'];
-        $d_name4 = $row[4]['d_name'];
+        $d_id1 = $row[1]["d_id"];
+        $d_id2 = $row[2]["d_id"];
+        $d_id3 = $row[3]["d_id"];
+        $d_id4 = $row[4]["d_id"];
+        $d_name1 = $row[1]["d_name"];
+        $d_name2 = $row[2]["d_name"];
+        $d_name3 = $row[3]["d_name"];
+        $d_name4 = $row[4]["d_name"];
         $row = $d_id1+$d_name1.$d_id2+$d_name2.$d_id3+$d_name3.$d_id4+$d_name4;
         }
     $replyText["type"] = "text";
