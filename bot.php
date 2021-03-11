fc086ecf552b4ac63f94a3f84b78b9c3a3e32bf3
Toey <71362543+the1999@users.noreply.github.com>
2021-03-11 16:34:42 +0700

    git up58

--- bot.php
+++ bot.php
@@ -77,14 +77,13 @@ else if ($text == "ดูรายชื่อพนักงาน") {
     $myPDO->query($sql_query2);
     
     $test = "";
+    $row1 = $row[1]['d_id']['d_name'];
+    $row2 = $row[2]['d_id']['d_name'];
+    $row3 = $row[3]['d_id']['d_name'];
+    $row4 = $row[4]['d_id']['d_name'];
+    $rows = $row1.$row2.$row3.$row4;
     foreach ($myPDO->query($sql_query2) as $rows) {
-        $row1 = $row[1]['d_id']['d_name'];
-        $row2 = $row[2]['d_id']['d_name'];
-        $row3 = $row[3]['d_id']['d_name'];
-        $row4 = $row[4]['d_id']['d_name'];
-        $rows = $row1.$row2.$row3.$row4;
         $test .= $rows;
-        
     } 
     echo $test;
     $replyText["type"] = "text";
