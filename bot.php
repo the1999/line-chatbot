59e0e0939d3b2d78d6196b21b54b84ba187865cc
Toey <71362543+the1999@users.noreply.github.com>
2021-03-11 16:28:02 +0700

    git up53

--- bot.php
+++ bot.php
@@ -79,7 +79,7 @@ else if ($text == "ดูรายชื่อพนักงาน") {
     $row2 = $row[2]['d_id']['d_name'];
     $row3 = $row[3]['d_id']['d_name'];
     $row4 = $row[4]['d_id']['d_name'];
-    $rows = array($row1.$row2.$row3.$row4);
+    $rows = $row1.$row2.$row3.$row4;
     $test = "";
     foreach ($myPDO->query($sql_query2) as $array) {
         $array .= $rows;
