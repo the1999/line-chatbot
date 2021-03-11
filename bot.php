962cee59e84c722619fa70e4f905e9ad38d340f0
Toey <71362543+the1999@users.noreply.github.com>
2021-03-11 15:41:19 +0700

    git up43

--- bot.php
+++ bot.php
@@ -75,10 +75,10 @@ else if (preg_match("/^((((19|[2-9]\d)\d{2})\-(0[13578]|1[02])\-(0[1-9]|[12]\d|3
 else if ($text == "ดูรายชื่อพนักงาน") {
     $sql_query2 = "SELECT * FROM tb_user";
     $myPDO->query($sql_query2);
-    $d_id1 = [1]['d_id']['d_name'];
-    $d_id2 = [2]['d_id']['d_name'];
-    $d_id3 = [3]['d_id']['d_name'];
-    $d_id4 = [4]['d_id']['d_name'];
+    $d_id1 = $row[1]['d_id']['d_name'];
+    $d_id2 = $row[2]['d_id']['d_name'];
+    $d_id3 = $row[3]['d_id']['d_name'];
+    $d_id4 = $row[4]['d_id']['d_name'];
     $rows = $d_id1.$d_id2.$d_id3.$d_id4;
     $test = "";
     foreach ($myPDO->query($sql_query2) as $rows) {
