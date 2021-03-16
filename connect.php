43a9edb873f79fe60de182fd92573edc83e81fc8
Toey <71362543+the1999@users.noreply.github.com>
2021-03-16 11:05:30 +0700

    day589

--- connect.php
+++ connect.php
@@ -4,7 +4,7 @@ try {
     $myPDO = new PDO("pgsql:host=ec2-52-44-31-100.compute-1.amazonaws.com;dbname=d9gbnhqsslvumi;user=cvjvcfygarrxpt;password=0f79bcb74f6de687639e6a9f73aa0cbd4ea4f21bfaecea939bc1afc3b9db0621");
     // $sql1 = "INSERT INTO tb_leave(user_id,d_date,d_detail) VALUES (1,'2021-03-15','ไปหาหมอ')";
     // $myPDO->query($sql1);
-    $sql = "SELECT * FROM tb_leave WHERE user_id=1";
+    $sql = "SELECT * FROM tb_leave WHERE user_id=2";
     foreach($myPDO->query($sql) as $row) {
         print "<br/>";
         print $row["user_id"].$row["d_date"].$row["d_detail"].'<br/>';
