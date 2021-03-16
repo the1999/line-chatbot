fbc93d778a1cf289727de3b23fd552eb338302d6
Toey <71362543+the1999@users.noreply.github.com>
2021-03-16 12:30:52 +0700

    day87

--- connect.php
+++ connect.php
@@ -9,7 +9,7 @@ try {
     //     print "<br/>";
     //     print $row["user_id"].$row["d_date"].$row["d_detail"].'<br/>';
     // }
-    $sql1 = "SELECT user_id,COUNT(*) FROM tb_leave ";
+    $sql1 = "SELECT COUNT(*) FROM tb_leave ";
     $myPDO->query($sql1);
     foreach($myPDO->query($sql1) as $row) {
         print "<br/>";
