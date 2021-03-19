7edbc34a1a04cab72d80f12e745d16103f6687a1
Toey <71362543+the1999@users.noreply.github.com>
2021-03-19 16:02:42 +0700

    qw39

--- bot.php
+++ bot.php
@@ -70,7 +70,7 @@ else if ($text == "2.ดูข้อมูลการลา") {
 }
 
 else if ($text == "$detail") {
-    $sql_query3 = "SELECT * FROM tb_leave WHERE user_id";
+    $sql_query3 = "SELECT * FROM tb_leave WHERE user_id=1";
     $myPDO->query($sql_query3); 
     $test1 = "";
     foreach ($myPDO->query($sql_query3) as $row) {
