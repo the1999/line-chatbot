a0e8586b5b278dedd2537bd0ad88e77fbeb8e862
Toey <71362543+the1999@users.noreply.github.com>
2021-03-19 16:16:43 +0700

    qw42

--- bot.php
+++ bot.php
@@ -46,7 +46,7 @@ else if ($text == "1.บันทึกการลา") {
 } 
 
 else if (preg_match("/^((((19|[2-9]\d)\d{2})\-(0[13578]|1[02])\-(0[1-9]|[12]\d|3[01]))|(((19|[2-9]\d)\d{2})\-(0[13456789]|1[012])\-(0[1-9]|[12]\d|30))|(((19|[2-9]\d)\d{2})\-02\-(0[1-9]|1\d|2[0-8]))|(((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))\-02\-29))$/",$massageArray[1])) {
-    $sql_query1 = "INSERT INTO tb_leave(user_id,d_date,d_detail) VALUES ('".user_id=$massageArray[0]."','".d_date=$massageArray[1]."','".d_detail$massageArray[2]."')";
+    $sql_query1 = "INSERT INTO tb_leave(user_id,d_date,d_detail) VALUES ('".$massageArray[0]."','".$massageArray[1]."','".$massageArray[2]."')";
     $myPDO->query($sql_query1);
         $message = '{
             "type" : "text",
