d338f0201558889574fcb3f33cfcfaa2efc173a3
Toey <71362543+the1999@users.noreply.github.com>
2021-03-22 14:15:48 +0700

    abc56

--- bot.php
+++ bot.php
@@ -54,6 +54,16 @@ elseif ($text == "ยกเลิกการลา") {
     }';
     $replyText = json_decode($message);
 }
+else if ($text == "abc") {
+    // $sql_query7 = "DELETE FROM tb_leave WHERE user_id = $delete[0] AND d_id = $delete[1]";
+    // $myPDO->query($sql_query7);  
+    $message = '{
+        "type" : "text",
+        "text" : "ยกเลิกการลาเรียบร้อย"
+    }';
+    $replyText = json_decode($message);
+}
+
 
 ///บันทึกการลา
 else if (preg_match("/^((((19|[2-9]\d)\d{2})\-(0[13578]|1[02])\-(0[1-9]|[12]\d|3[01]))|(((19|[2-9]\d)\d{2})\-(0[13456789]|1[012])\-(0[1-9]|[12]\d|30))|(((19|[2-9]\d)\d{2})\-02\-(0[1-9]|1\d|2[0-8]))|(((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))\-02\-29))$/",$massageArray[1])) {
@@ -94,15 +104,6 @@ else if (!empty($detail[0])) {
 
 } 
 
-else if ($text == "abc") {
-    // $sql_query7 = "DELETE FROM tb_leave WHERE user_id = $delete[0] AND d_id = $delete[1]";
-    // $myPDO->query($sql_query7);  
-    $message = '{
-        "type" : "text",
-        "text" : "ยกเลิกการลาเรียบร้อย"
-    }';
-    $replyText = json_decode($message);
-}
 
 
 $lineData['URL'] = "https://api.line.me/v2/bot/message/reply";
