b75deedaaeae594cd192d343deea5bec45f2d8b2
Toey <71362543+the1999@users.noreply.github.com>
2021-03-22 14:22:46 +0700

    abc62

--- bot.php
+++ bot.php
@@ -46,8 +46,23 @@ else if ($text == "บันทึกการลา") {
         "text" : "คุณต้องการลาวันที่เท่าไหร่  \n ตัวอย่างเช่น ใส่IDของคุณ,2021-01-01,ไปหาหมอ"
     }';
     $replyText = json_decode($message);
-} 
-
+}
+elseif ($text == "ยกเลิกการลา") {
+    $message = '{
+        "type" : "text",
+        "text" : "คุณต้องการยกเลิกการลาวันไหน  \n ตัวอย่างเช่น ใส่User IDของคุณ,ใส่IDวันที่ต้องการลา"
+    }';
+    $replyText = json_decode($message);
+}
+else if ($text == isset($delete[0])) {
+    $sql_query7 = "DELETE FROM tb_leave WHERE user_id = $delete[0] AND d_id = $delete[1]";
+    $myPDO->query($sql_query7);  
+    $message = '{
+        "type" : "text",
+        "text" : "ยกเลิกการลาเรียบร้อย"
+    }';
+    $replyText = json_decode($message);
+}
 
 ///บันทึกการลา
 else if (preg_match("/^((((19|[2-9]\d)\d{2})\-(0[13578]|1[02])\-(0[1-9]|[12]\d|3[01]))|(((19|[2-9]\d)\d{2})\-(0[13456789]|1[012])\-(0[1-9]|[12]\d|30))|(((19|[2-9]\d)\d{2})\-02\-(0[1-9]|1\d|2[0-8]))|(((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))\-02\-29))$/",$massageArray[1])) {
@@ -87,22 +102,6 @@ else if (!empty($detail[0])) {
     $replyText["text"] = "$test1";
 
 } 
-elseif ($text == "ยกเลิกการลา") {
-    $message = '{
-        "type" : "text",
-        "text" : "คุณต้องการยกเลิกการลาวันไหน  \n ตัวอย่างเช่น ใส่User IDของคุณ,ใส่IDวันที่ต้องการลา"
-    }';
-    $replyText = json_decode($message);
-}
-else if ($text == isset($delete[0])) {
-    $sql_query7 = "DELETE FROM tb_leave WHERE user_id = $delete[0] AND d_id = $delete[1]";
-    $myPDO->query($sql_query7);  
-    $message = '{
-        "type" : "text",
-        "text" : "ยกเลิกการลาเรียบร้อย"
-    }';
-    $replyText = json_decode($message);
-}
 
 
 
