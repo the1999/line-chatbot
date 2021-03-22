fa41e3902aca3872d09efa329ed6cbed17a6cc10
Toey <71362543+the1999@users.noreply.github.com>
2021-03-22 09:58:37 +0700

    abc10

--- bot.php
+++ bot.php
@@ -91,15 +91,15 @@ else if ($text == "3.ยกเลิกการลา") {
     }';
     $replyText = json_decode($message);
 }
-// else if ($delete[0]) {
-//     $sql_query7 = "DELETE FROM tb_leave WHERE user_id = $delete[0] AND d_id = $delete[1]";
-//     $myPDO->query($sql_query7);  
-//     $message = '{
-//         "type" : "text",
-//         "text" : "ยกเลิกการลาเรียบร้อย"
-//     }';
-//     $replyText = json_decode($message);
-// }
+else if ($text == isset($delete[0])) {
+    $sql_query7 = "DELETE FROM tb_leave WHERE user_id = $delete[0] AND d_id = $delete[1]";
+    $myPDO->query($sql_query7);  
+    $message = '{
+        "type" : "text",
+        "text" : "ยกเลิกการลาเรียบร้อย"
+    }';
+    $replyText = json_decode($message);
+}
 
 
 $lineData['URL'] = "https://api.line.me/v2/bot/message/reply";
