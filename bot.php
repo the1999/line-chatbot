7034a8526a2e7e2f73eab0a0977647a245ff3048
Toey <71362543+the1999@users.noreply.github.com>
2021-03-23 14:16:48 +0700

    b38

--- bot.php
+++ bot.php
@@ -90,7 +90,7 @@ else if (!empty($detail[0])) {
 } 
 
 else if (!empty($delete[0])) {
-    $sql_query4 = "SELECT * FROM tb_leave WHERE user_id=$delete[0], d_id=$delete[1]";
+    $sql_query4 = "SELECT * FROM tb_leave WHERE user_id=$delete[0] AND d_id=$delete[1]";
     $myPDO->query($sql_query4);
     $message '{
         "type": "text",
