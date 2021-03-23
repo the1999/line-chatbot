ee1a1c266707187169712152b3d70c0aa2f2d23c
Toey <71362543+the1999@users.noreply.github.com>
2021-03-23 09:49:54 +0700

    b4

--- bot.php
+++ bot.php
@@ -89,11 +89,12 @@ else if (!empty($detail[0])) {
 
 } 
 elseif ($text == "ยกเลิกการลา") {
-    $message = '{
-        "type" : "text",
-        "text" : "คุณต้องการยกเลิกการลาวันไหน  \n ตัวอย่างเช่น ใส่User IDของคุณ,ใส่IDวันที่ต้องการลา"
-    }';
-    $replyText = json_decode($message);
+    $text1 = "คุณต้องการยกเลิกการลาวันไหน  \n ตัวอย่างเช่น ใส่User IDของคุณ,ใส่IDวันที่ต้องการลา";
+    echo $text1;
+    
+    $replyText["type"] = "text";
+    $replyText["text"] = "$text1";
+
 }
 else if (!empty($delete[0])) {
     $sql_query7 = "DELETE FROM tb_leave WHERE user_id = $delete[0] AND d_id = $delete[1]";
